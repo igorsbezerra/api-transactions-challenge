1a73e0071177ca6941ca936daa20eaeae08d5922
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2024-01-12 18:57:55 -0300

    init api

---
+++ src/test/java/dev/igor/apitransactions/ApiTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.igor.apitransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
